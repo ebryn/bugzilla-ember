01d98f020f68936a1608b7e2898a98908b791446
Erik Bryn <erik.bryn@gmail.com>
2013-06-15 23:56:50 -0400

    Promises bug as default :D

--- app/assets/javascripts/routes/index_route.js
+++ app/assets/javascripts/routes/index_route.js
@@ -1,7 +1,7 @@
 App.IndexRoute = Ember.Route.extend({
   redirect: function() {
-    var bug = App.Bug.find(221820);
-    bug.set('id', 221820); // FIXME: why isn't the ID loaded?
+    var bug = App.Bug.find(856410);
+    bug.set('id', 856410); // FIXME: why isn't the ID loaded?
 
     this.transitionTo("bug", bug);
   }
