f8d3e60d9ad8d16f935927afa8d5bfc100ba5baf
Erik Bryn <erik.bryn@gmail.com>
2013-06-18 20:40:22 -0500

    Show flags

--- app/assets/javascripts/models/bug.js
+++ app/assets/javascripts/models/bug.js
@@ -24,6 +24,7 @@ App.Bug = Ember.Model.extend({
   creation_time: attr(Date),
   last_change_time: attr(Date),
   cc: attr(),
+  flags: attr(),
 
   aliasOrId: function() {
     return this.get('alias') || this.get('id');
