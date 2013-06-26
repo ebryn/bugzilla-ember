9ed89320477f4e43813045eb306e1ca30da37139
Jesse Patel <jessep@gmail.com>
2013-06-25 19:31:04 -0700

    Adding browse table with sortable columns.

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -2,4 +2,5 @@ App.Router.map(function() {
   this.route("bug", {path: "bug/:bug_id"});
   this.route("attachment", {path: "attachment/:attachment_id"});
   this.route("dashboard");
+  this.route("browse");
 });
