4e7ce3668d5d1d3aa4191a2334c2d5fda5d554cc
Erik Bryn <erik.bryn@gmail.com>
2013-07-22 21:59:09 -0500

    Move BugAdapter out to it's own file

---
+++ app/adapters/bug.js
@@ -0,0 +1,75 @@
+import getJSON from 'bugzilla/utils/get_json';
+import ajax from 'bugzilla/utils/ajax';
+import urlFor from 'bugzilla/utils/url_for';
+import promiseStorage from 'bugzilla/utils/promise_storage';
+import unhandledRejection from 'bugzilla/utils/unhandled_rejection';
+
+var Adapter = Ember.Adapter.extend({
+  find: function(record, id) {
+    var self = this;
+
+    return promiseStorage.getItem('bug-' + id).then(function(value){
+      if (value !== null) {
+        record.load(id, value);
+
+        return self._getJSON(id, {include_fields: "last_change_time"}).then(function(json) {
+          if (json.bugs[0].last_change_time !== value.last_change_time) {
+            self._loadFromServer(record, id);
+          }
+        });
+
+      } else {
+        return self._loadFromServer(record, id);
+      }
+    }).then(null, unhandledRejection);
+  },
+
+  findMany: function(klass, records, ids) {
+    var idParams = ids.map(function(id) { return "id=" + id; }).join('&');
+    return this._getJSON("", idParams).then(function(data) {
+      records.load(klass, data.bugs);
+    });
+  },
+
+  findQuery: function(klass, records, params) {
+    this._getJSON("", params).then(function(data) {
+      records.load(klass, data.bugs);
+    });
+  },
+
+  createRecord: function(record) {
+    var url = urlFor("bug");
+    return ajax(url, {
+      type: "POST",
+      dataType: 'json',
+      contentType: 'application/json',
+      data: JSON.stringify(record.toJSON())
+    }).then(function(json) {
+      // FIXME: EM should be able to load just an ID
+      record.set('id', json.id);
+      record.didCreateRecord();
+      record.set('id', json.id);
+
+      return record;
+    }, function(err) {
+      var xhr = err[0],
+          json = xhr.responseJSON;
+      alert(json.message); // TODO: better error handling
+      
+      throw err;
+    });
+  },
+
+  _getJSON: function(id, params) {
+    return getJSON(urlFor("bug" + (id ? "/" + id : "")), params);
+  },
+
+  _loadFromServer: function(record, id) {
+    this._getJSON(id).then(function(json) {
+      var data = json.bugs[0];
+      record.load(id, data);
+    });
+  }
+});
+
+export default Adapter;
--- app/models/bug.js
+++ app/models/bug.js
@@ -1,11 +1,8 @@
 import Comment from 'bugzilla/models/comment';
 import Attachment from 'bugzilla/models/attachment';
+import BugAdapter from 'bugzilla/adapters/bug';
 
-import getJSON from 'bugzilla/utils/get_json' ;
-import ajax from 'bugzilla/utils/ajax' ;
-import urlFor from 'bugzilla/utils/url_for'  ;
 import promiseStorage from 'bugzilla/utils/promise_storage' ;
-import unhandledRejection from 'bugzilla/utils/unhandled_rejection';
 
 // TODO: remove app dependency
 import App from 'bugzilla/app';
@@ -154,66 +151,7 @@ Bug.reopenClass({
     });
   },
 
-  adapter: Ember.Adapter.create({
-    _getJSON: function(id, params) {
-      return getJSON(urlFor("bug" + (id ? "/" + id : "")), params);
-    },
-
-    _loadFromServer: function(record, id) {
-      this._getJSON(id).then(function(json) {
-        var data = json.bugs[0];
-        record.load(id, data);
-      });
-    },
-
-    find: function(record, id) {
-      var self = this;
-
-      return promiseStorage.getItem('bug-' + id).then(function(value){
-        if (value !== null) {
-          record.load(id, value);
-
-          return self._getJSON(id, {include_fields: "last_change_time"}).then(function(json) {
-            if (json.bugs[0].last_change_time !== value.last_change_time) {
-              self._loadFromServer(record, id);
-            }
-          });
-
-        } else {
-          return self._loadFromServer(record, id);
-        }
-      }).then(null, unhandledRejection);
-    },
-
-    findMany: function(klass, records, ids) {
-      var idParams = ids.map(function(id) { return "id=" + id; }).join('&');
-      return this._getJSON("", idParams).then(function(data) {
-        records.load(klass, data.bugs);
-      });
-    },
-
-    findQuery: function(klass, records, params) {
-      this._getJSON("", params).then(function(data) {
-        records.load(klass, data.bugs);
-      });
-    },
-
-    createRecord: function(record) {
-      var url = urlFor("bug");
-      return ajax(url, {
-        type: "POST",
-        dataType: 'json',
-        contentType: 'application/json',
-        data: JSON.stringify(record.toJSON())
-      }).then(function(json) {
-        record.set('id', parseInt(json.id, 10));
-        record.didCreateRecord();
-      }, function(xhr) {
-        var json = xhr.responseJSON;
-        alert(json.message); // TODO: better error handling
-      });
-    }
-  })
+  adapter: BugAdapter.create()
 });
 
 export default Bug;
