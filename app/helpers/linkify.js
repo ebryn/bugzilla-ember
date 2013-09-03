94a184e4fc2552bcefaf60ac360b4be77ab8f41b
Erik Bryn <erik.bryn@gmail.com>
2013-09-03 11:28:51 -0500

    Handle null values in linkify helper

--- app/helpers/linkify.js
+++ app/helpers/linkify.js
@@ -2,5 +2,5 @@ import linkify from "bugzilla/utils/linkify";
 
 Ember.Handlebars.helper('linkify', function(value) {
   // FIXME: Is the backend sanitizing for HTML? If not, we shouldn't be marking this whole string as safe.
-  return new Handlebars.SafeString(linkify(value));
+  return new Handlebars.SafeString(linkify(value || ""));
 });
