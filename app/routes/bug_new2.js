e14c59f60861c1204e28f7ec20a6589343933667
Erik Bryn <erik.bryn@gmail.com>
2013-09-04 14:37:20 -0500

    Update bug creation form
    Revert back to using statically defined fields in the create form. Only custom fields are dynamically included now.

--- app/routes/bug_new2.js
+++ app/routes/bug_new2.js
@@ -3,14 +3,30 @@ import getJSON from "bugzilla/utils/get_json";
 
 var Route = Ember.Route.extend({
   model: function(params) {
-    return getJSON(urlFor("ember/create/" + params.product));
+    return getJSON(urlFor("ember/create/" + params.product).replace(/\?.*$/, '') /* FIXME: remove replace when create API is fixed */).then(function(json) {
+      var ret = {
+        product: json.product,
+        fields: {},
+        customFields: []
+      };
+
+      json.fields.forEach(function(field) {
+        if (field.is_custom) {
+          ret.customFields.push(field);
+        } else {
+          ret.fields[field.name] = field;
+        }
+      });
+
+      return ret;
+    });
   },
 
   setupController: function(controller, model) {
     controller.set('content', this.create('bug'));
-    // controller.set('content', model);
     controller.set('product', model.product);
-    controller.set('fields', model.fields);
+    controller.set('fields',  model.fields);
+    controller.set('customFields', model.customFields);
   }
 });
 
