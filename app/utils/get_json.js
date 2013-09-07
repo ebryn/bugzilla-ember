28e0a5beb71127b3754248dfc6a4dc525518be8a
Erik Bryn <erik.bryn@gmail.com>
2013-09-07 10:19:34 -0500

    Reject faux successful XHRs
    The API will return a successful HTTP status, but the response contains an error

--- app/utils/get_json.js
+++ app/utils/get_json.js
@@ -2,10 +2,16 @@ function getJSON(url, params) {
   var args = arguments;
 
   return new Ember.RSVP.Promise(function(resolve, reject){
-    $.getJSON.apply($, args).then(function(value) {
-      Ember.run(null, resolve, value);
-    }, function(reason) {
-      Ember.run(null, reject, arguments);
+    $.getJSON.apply($, args).then(function(json, status, xhr) {
+      if (json.error) {
+        // avoid issue with successfully resolved xhr promise causing rejection to fail
+        xhr.then = null;
+        Ember.run(null, reject, xhr);
+      } else {
+        Ember.run(null, resolve, json);
+      }
+    }, function(xhr) {
+      Ember.run(null, reject, xhr);
     });
   });
 }
