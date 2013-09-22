fc08614dc1d0b6c0491f2b06dace27f91e1645ac
Erik Bryn <erik.bryn@gmail.com>
2013-09-22 10:53:12 -0500

    quick search WIP

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -3,10 +3,17 @@ import Bug from 'bugzilla/models/bug';
 var SearchControler = Ember.Controller.extend({
   searchText: null,
 
-  searchResults: function() {
-    var searchText = this.get('searchText');
-    return Bug.search(searchText);
-  }.property('searchText'),
+  actions: {
+    quicksearch: function() {
+      window.open(this.get('config.host') + "/buglist.cgi?quicksearch=" + this.get('searchText'));
+      this.clearSearchText();
+    }
+  },
+
+  // searchResults: function() {
+  //   var searchText = this.get('searchText');
+  //   return Bug.search(searchText);
+  // }.property('searchText'),
 
   clearSearchText: function() {
     this.set('searchText', null);
