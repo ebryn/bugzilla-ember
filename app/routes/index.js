e1a78380409e0a7fc9067edf3b16345d0a304093
Erik Bryn <erik.bryn@gmail.com>
2013-09-27 10:45:33 -0500

    Fix index redirect

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,7 @@
 var IndexRoute = Ember.Route.extend({
   redirect: function() {
     var id = this.get('config.defaultBugId');
-    var bug = this.find('bug', id);
-
-    bug.set('id', id); // FIXME: why isn't the ID loaded?
-    this.transitionTo("bug", bug);
+    this.transitionTo("bug", id);
   }
 });
 
