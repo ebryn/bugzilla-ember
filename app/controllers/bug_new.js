1b1018d936000aae727bad7ade622f10d3f96168
Erik Bryn <erik.bryn@gmail.com>
2013-11-07 17:18:34 -0600

    Improvements to bug creation page

--- app/controllers/bug_new.js
+++ app/controllers/bug_new.js
@@ -1,5 +1,6 @@
 import Bug from 'bugzilla/models/bug';
 import unhandledRejection from 'bugzilla/utils/unhandled_rejection';
+import Flag from 'bugzilla/models/flag';
 
 var Controller = Ember.ArrayController.extend({
   selectedProduct: null,
@@ -10,6 +11,8 @@ var Controller = Ember.ArrayController.extend({
   newBug: null,
   newBugIsLoading: false,
   fields: Em.computed.alias('newBug.fields'),
+  projectFlags: Em.computed.alias('newBug.projectFlags'),
+  trackingFlags: Em.computed.alias('newBug.trackingFlags'),
 
   _selectedProductDidChange: function() {
     var selectedProduct = this.get('selectedProduct.name'),
@@ -70,12 +73,75 @@ var Controller = Ember.ArrayController.extend({
 
   severity: function(key, value) {
     if (arguments.length === 2) {
-      this.set('model.fields.severity.currentValue', value);
+      this.set('fields.severity.currentValue', value);
       return value;
     }
 
     return this.get('fields.severity.defaultValue');
   }.property('fields.severity.defaultValue'),
+
+  newFlags: function() {
+    var allFlags = this.get('fields.flags.values'),
+        existingFlags = this.get('fields.flags.currentValue'),
+        settableFlags = [];
+
+    allFlags.forEach(function(flag) {
+      var flagExists = existingFlags.findProperty('name', flag.get('name'));
+      // if flag exists and !is_multiplicable
+      if (flagExists && !flag.get('isMultiplicable')) {
+        // not settable
+      } else {
+        // convert flags into a model?
+        settableFlags.push(flag);
+      }
+    });
+
+    return settableFlags;
+  }.property('fields.flags.values.[]', 'fields.flags.currentValue.[]'),
+
+  shouldShowNewFlagRequestee: function() {
+    return this.get('newFlagDefinition.isRequesteeble') && this.get('newFlagStatus') === "?";
+  }.property('newFlagDefinition.isRequesteeble', 'newFlagStatus'),
+
+  newTrackingFlags: function() {
+    var allFlags = this.get('trackingFlags.values'),
+        existingFlags = this.get('trackingFlags.currentValue'),
+        settableFlags = [];
+
+    allFlags.forEach(function(flag) {
+      var flagExists = existingFlags.findProperty('name', flag.name);
+      // all tracking flags are not multiplicable
+      if (!flagExists) {
+        settableFlags.push(flag);
+      }
+    });
+
+    return settableFlags;
+  }.property('trackingFlags.values.[]', 'trackingFlags.currentValue.[]'),
+
+  newProjectFlags: function() {
+    var allFlags = this.get('projectFlags.values'),
+        existingFlags = this.get('projectFlags.currentValue'),
+        settableFlags = allFlags.slice();
+
+    existingFlags.forEach(function(group) {
+      settableFlags.removeObject(group);
+    });
+
+    return settableFlags;
+  }.property('projectFlags.values.[]', 'projectFlags.currentValue.[]'),
+
+  newGroups: function() {
+    var allGroups = this.get('fields.groups.values'),
+        existingGroups = this.get('fields.groups.currentValue'),
+        settableGroups = allGroups.slice();
+
+    existingGroups.forEach(function(group) {
+      settableGroups.removeObject(group);
+    });
+
+    return settableGroups;
+  }.property('fields.groups.values.[]', 'fields.groups.currentValue.[]'),
 
   actions: {
     toggleAdvanced: function() {
@@ -91,6 +157,7 @@ var Controller = Ember.ArrayController.extend({
       model.create().then(function(newBugId) {
         self.set('isSaving', false);
         self.set('flashMessage', null);
+        // self.set('selectedProduct', null);
         self.transitionToRoute("bug", newBugId);
       }, function(reason) {
         self.set('isSaving', false);
@@ -104,7 +171,73 @@ var Controller = Ember.ArrayController.extend({
           console.log(reason);
         }
       });
-    }
+    },
+
+    addNewFlag: function() {
+      var flagDefinition = this.get('newFlagDefinition'),
+          flagStatus = this.get('newFlagStatus');
+
+      if (!flagDefinition || !flagStatus) { return; }
+
+      var currentFlags = this.get('fields.flags.currentValue'),
+          newFlag = Flag.fromDefinition(flagDefinition, {
+            status: flagStatus,
+            requestee: {email: this.get('newFlagRequestee')}
+          });
+
+      currentFlags.pushObject(newFlag);
+
+      this.setProperties({
+        newFlagDefinition: null,
+        newFlagStatus: null,
+        newFlagRequestee: null
+      });
+    },
+
+    addNewTrackingFlag: function() {
+      var flagDefinition = this.get('newTrackingFlag'),
+          flagStatus = this.get('newTrackingFlagStatus');
+
+      if (!flagDefinition || !flagStatus) { return; }
+
+      var currentFlags = this.get('trackingFlags.currentValue'),
+          newFlag = Ember.merge(flagDefinition, {currentValue: flagStatus});
+
+      currentFlags.pushObject(newFlag);
+
+      this.setProperties({
+        newTrackingFlag: null,
+        newTrackingFlagStatus: null
+      });
+    },
+
+    addNewProjectFlag: function() {
+      var flagDefinition = this.get('newProjectFlag'),
+          flagStatus = this.get('newProjectFlagStatus');
+
+      if (!flagDefinition || !flagStatus) { return; }
+
+      var currentFlags = this.get('projectFlags.currentValue'),
+          newFlag = Ember.merge(flagDefinition, {currentValue: flagStatus});
+
+      currentFlags.pushObject(newFlag);
+
+      this.setProperties({
+        newProjectFlag: null,
+        newProjectFlagStatus: null
+      });
+    },
+
+    addNewGroup: function() {
+      var group = this.get('newGroup');
+
+      if (!group) { return; }
+
+      var currentGroups = this.get('fields.groups.currentValue');
+      currentGroups.pushObject(group);
+
+      this.setProperties({newGroup: null});
+    },
   }
 });
 
