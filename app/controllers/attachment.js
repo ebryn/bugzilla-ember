89f3c74861a809f95fca7799077d46c3e77e92b6
Erik Bryn <erik.bryn@gmail.com>
2013-11-07 10:39:50 -0600

    Fix attachment review links

--- app/controllers/attachment.js
+++ app/controllers/attachment.js
@@ -1,7 +1,7 @@
 import fmt from 'bugzilla/utils/fmt';
 
 var AttachmentController = Ember.ObjectController.extend({
-  reviewURL: fmt("%@/page.cgi?id=splinter.html&bug=%@&attachment=%@", 'config.host', 'bugId', 'id'),
+  reviewURL: fmt("%@/page.cgi?id=splinter.html&bug=%@&attachment=%@", 'config.host', 'bug_id', 'id'),
   diffURL:   fmt("%@/attachment.cgi?id=%@&action=diff", 'config.host', 'id')
 });
 
